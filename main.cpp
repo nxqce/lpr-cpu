d5dd6d4d76ffbf958b232ab1de45d4572a5c7114
nxqce <nvtnxq@gmail.com>
2018-03-07 10:32:09 +0700

    regconize the numbers

--- main.cpp
+++ main.cpp
@@ -11,6 +11,8 @@ using namespace cv;
 
 double xSolve(double a1, double b1, double c1, double a2, double b2, double c2);
 double ySolve(double a1, double b1, double c1, double a2, double b2, double c2);
+int dec2bin(int num);
+char getChar(int code);
 
 int main(int argc, char** argv) {
 	Mat input;
@@ -134,7 +136,7 @@ int main(int argc, char** argv) {
 						}
 					}
 
-					if (black > 0 && white / black >= 2)
+					if (black > 0 && white / black >= 2 && plateTempImg.rows > 200)
 						plateImg = plateTempImg;
 				}
 			}
@@ -146,31 +148,113 @@ int main(int argc, char** argv) {
 	//cvtColor(plateImg, plateGrayImg, CV_BGR2GRAY);
 	//threshold(plateGrayImg, plateBinImg, 120, 255, CV_THRESH_BINARY);
 	resize(plateImg, plateImg, Size(270, 200));
-	Mat plateBlurImg, plateEdgeImg;
+	Mat plateBlurImg, plateEdgeImg, plateBinImg, plateGrayImg, plateContourImg;
 	//GaussianBlur(plateImg, plateBlurImg, Size(3, 3), 0);
-	Canny(plateImg, plateEdgeImg, 50, 300, 3) ;
+	//Canny(plateImg, plateEdgeImg, 50, 300, 3) ;
 	//Mat morpho;
-	//Mat element = getStructuringElement(MORPH_CROSS, Size(3, 3), Point(1, 1));
+	Mat element = getStructuringElement(MORPH_ELLIPSE, Size(3, 3), Point(1, 1));
 	//erode(plateImg, morpho, element, Point(-1, -1), 3);
 	//imshow("Erode", morpho);
 	//cvtColor(morpho, morpho, CV_BGR2GRAY);
 	//threshold(morpho, morpho, 110, 255, CV_THRESH_BINARY);
+	
+	cvtColor(plateImg, plateGrayImg, CV_BGR2GRAY);
+	threshold(plateGrayImg, plateBinImg, 160, 255, CV_THRESH_BINARY);
+	dilate(plateBinImg, plateBinImg, element);
+	plateContourImg = plateBinImg.clone();
+	imshow("Bin", plateBinImg);
+
 	vector < vector<Point> > contours;
-	findContours(plateEdgeImg, contours, CV_RETR_LIST, CV_CHAIN_APPROX_NONE);
+	findContours(plateContourImg, contours, CV_RETR_LIST, CV_CHAIN_APPROX_NONE);
+
+	Mat charImg[9];
+	int index = 0;
+	/// Approximate contours to polygons + get bounding rects and circles
 	vector<vector<Point> > contours_poly( contours.size() );
-	for (size_t i = 0; i < contours.size(); i++) {
-		//Rect r = boundingRect(contours[i]);
+	vector<Rect> boundRect( contours.size() );
+
+	for( int i = 0; i < contours.size(); i++ ) {
 		approxPolyDP( Mat(contours[i]), contours_poly[i], 3, true );
-       	//boundRect[i] = boundingRect( Mat(contours_poly[i]) );
-		Rect r = boundingRect( Mat(contours_poly[i]) );
-		if ((r.width / (double)r.height > 0.2f && r.width / (double)r.height < 0.5f)
-			&& (r.height < 100 && r.height > 70))
-			rectangle(plateImg, r, Scalar(0, 0, 255), 1, 8, 0);
-		
+		boundRect[i] = boundingRect( Mat(contours_poly[i]) );
 	}
 
-	imshow("Line", lineImg);
-	imshow("Edge", plateEdgeImg);
+	/// Draw bonding rects
+	// GaussianBlur(plateImg, plateBlurImg, Size(3, 3), 0);
+	// Canny(plateBlurImg, plateEdgeImg, 50, 300, 3);
+
+	for( int i = 0; i< contours.size(); i++ ) {
+		Rect r = boundRect[i];
+		if ((r.width / (double)r.height > 0.2f && r.width / (double)r.height < 0.5f)
+			&& (r.height < 100 && r.height > 70)){
+			
+			rectangle( plateImg, r, Scalar(0, 0, 255), 1, 8, 0 );
+			charImg[index] = plateBinImg(r);
+			index++;
+		}
+	}
+
+	cout << "So ky tu: " << index << endl;
+
+	int im = index - 1;
+	while (im >= 0) {
+	// int im = 8;
+		// cout << charImg[im].cols << " : " << charImg[im].rows << endl;
+		line(charImg[im], Point(charImg[im].cols/3, 0), Point(charImg[im].cols/3, charImg[im].rows), Scalar(255, 255, 255), 1, CV_AA);
+		line(charImg[im], Point(charImg[im].cols/3*2, 0), Point(charImg[im].cols/3*2, charImg[im].rows), Scalar(255, 255, 255), 1, CV_AA);
+		line(charImg[im], Point(0, charImg[im].rows/6), Point(charImg[im].cols, charImg[im].rows/6), Scalar(255, 255, 255), 1, CV_AA);
+		line(charImg[im], Point(0, charImg[im].rows/6*2), Point(charImg[im].cols, charImg[im].rows/6*2), Scalar(255, 255, 255), 1, CV_AA);
+		line(charImg[im], Point(0, charImg[im].rows/6*3), Point(charImg[im].cols, charImg[im].rows/6*3), Scalar(255, 255, 255), 1, CV_AA);
+		line(charImg[im], Point(0, charImg[im].rows/6*4), Point(charImg[im].cols, charImg[im].rows/6*4), Scalar(255, 255, 255), 1, CV_AA);
+		line(charImg[im], Point(0, charImg[im].rows/6*5), Point(charImg[im].cols, charImg[im].rows/6*5), Scalar(255, 255, 255), 1, CV_AA);
+		imshow("num " + char(im), charImg[im]);
+
+		int charSquare[3][6] = {{0}};
+		int charBit = 0;
+		char regChar = '-';
+		
+		int black = 0, white = 0;
+		for (int l = 0; l < 6; l++) {
+			for (int c = 0; c < 3; c++){
+				white = 0;
+				black = 0;
+				for (int i = 0 + c*charImg[im].cols/3; i < (c + 1)*charImg[im].cols/3; i++) {
+					for (int j = 0 + l*charImg[im].rows/6; j < (l + 1)*charImg[im].rows/6; j++) {
+						//cout << (int)charImg[0].at<uchar>(Point(i, j)) << " ";
+						if ((int)charImg[im].at<uchar>(Point(i, j)) == 0) black++;
+						else white++;
+					}
+					//cout << endl;
+				}
+				// cout << "[" << l << "," << c << "]" << endl;
+				// cout << "black: " << black << " - white: " << white << endl;
+				// cout << "---------------------------------" << endl;
+				if (black > white / 4) {
+					charSquare[c][l] = 1;
+					charBit += pow(2,l*3 + c);
+				}
+				//charBit += charSquare[r][l]*pow(2,l*3 + r);
+			}
+		}
+
+		//dec2bin(charBit);
+
+		if (charImg[im].cols < 30){
+			regChar = '1';
+		}
+		else {
+			regChar = getChar(charBit);
+		}
+
+		//cout << charBit << endl;
+		cout << regChar;
+
+		im--;
+	}
+	cout << endl;
+	
+
+	//imshow("Line", lineImg);
+	//imshow("Edge", plateEdgeImg);
 	//imshow("Morpho", morpho);
 	imshow("Plate", plateImg);
 
@@ -192,4 +276,57 @@ double ySolve(double a1, double b1, double c1, double a2, double b2, double c2) 
 	double dy = c2*a1 - c1*a2;
 	if (dy == 0 || dy == 0) return -1;
 	return dy / d;
+}
+
+int dec2bin(int num){
+	int total = 0;
+	int count = 0;
+	while(num > 0)
+	{  
+		total = num % 2;  
+		num /= 2;
+		if (total == 1)
+			cout << total << " ";
+		else
+			cout << "  ";
+		if (count % 3 == 2)
+			cout << endl;
+		count++;
+	}	
+	cout << endl;
+	return 0;
+}
+
+char getChar(int code){
+	//define minimun code of number
+	int zero	= 88938,
+		two 	= 234786,
+		three 	= 84258,
+		four	= 160338,
+		five	= 116943,
+		six		= 88778,
+		seven	= 38023,
+		eight	= 86186, //43050,
+		nine	= 85354;
+
+	//compare the code with the minimun code
+	//8 is close to 0
+	if ((int)(code & eight) == eight) return '8';
+	else if ((int)(code & zero) == zero) return '0';
+
+	else if ((int)(code & two) == two) return '2';
+
+	//9 is close to 3
+	else if ((int)(code & nine) == nine) return '9';
+	else if ((int)(code & three) == three) return '3';
+
+	else if ((int)(code & four) == four) return '4';
+	
+	//6 is close to 5
+	else if ((int)(code & six) == six) return '6';
+	else if ((int)(code & five) == five) return '5';
+	
+	else if ((int)(code & seven) == seven) return '7';
+	
+	else return ' ';
 }
